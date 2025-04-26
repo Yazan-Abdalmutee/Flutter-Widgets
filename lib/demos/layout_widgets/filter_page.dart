1c48ec11fe1eb4f743405acfe0d91140b2272b0f
Yazan-Almutte-CE <yazan.abd.almutte@gmail.com>
2025-04-26 14:10:13 +0300

    initial commit

--- lib/demos/layout_widgets/filter_page.dart
+++ lib/demos/layout_widgets/filter_page.dart
@@ -33,181 +33,179 @@ class _FilterPageState extends State<FilterPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
-      body: ColoredBox(
-        color: Colors.deepPurple,
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(20),
-          child: ColoredBox(
-            color: mainColor,
-            child: Padding(
-              padding: const EdgeInsets.all(50.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        "Filter",
-                        style: TextStyle(fontSize: 50, color: fontColor),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.close, color: fontColor),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 30),
-                  Text(
-                    "SEARCH BY KEYWORD",
-                    style: TextStyle(fontSize: 20, color: fontColor),
-                  ),
-                  SizedBox(
-                    width: double.infinity,
-                    height: 60,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintStyle: TextStyle(color: fontColor),
-                        filled: true,
-
-                        fillColor: Color(0xFF242046),
-                        prefixIconColor: fontColor,
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10),
-                        ),
-                        hintText: "Search jobs",
-
-                        prefixIcon: Icon(Icons.search),
-                      ),
+      backgroundColor:   Colors.deepPurple,
+      body: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: ColoredBox(
+          color: mainColor,
+          child: Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "Filter",
+                      style: TextStyle(fontSize: 50, color: fontColor),
                     ),
-                  ),
-                  SizedBox(height: 30),
-                  Text(
-                    "View",
-                    style: TextStyle(fontSize: 30, color: fontColor),
-                  ),
-                  Row(
-                    children: [
-                      Transform.scale(
-                        scale: 1.4,
-                        child: Radio<Choices>(
-                          value: Choices.choice1,
-                          groupValue: _choice,
-                          activeColor: buttonsColor,
-                          onChanged: (Choices? value) {
-                            setState(() {
-                              _choice = value;
-                            });
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 15),
-                      Text(
-                        'All job posts',
-                        style: TextStyle(fontSize: 20, color: fontColor),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Transform.scale(
-                        scale: 1.4,
-
-                        child: Radio<Choices>(
-                          value: Choices.choice2,
-                          groupValue: _choice,
-                          activeColor: buttonsColor,
-                          onChanged: (Choices? value) {
-                            setState(() {
-                              _choice = value;
-                            });
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 15),
-                      Text(
-                        'Active job posts',
-                        style: TextStyle(fontSize: 20, color: fontColor),
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Transform.scale(
-                        scale: 1.4,
-                        child: Radio<Choices>(
-                          activeColor: buttonsColor,
-                          value: Choices.choice3,
-                          groupValue: _choice,
-                          onChanged: (Choices? value) {
-                            setState(() {
-                              _choice = value;
-                            });
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 15),
-                      Text(
-                        'Archive job Posts',
-                        style: TextStyle(fontSize: 20, color: fontColor),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 20),
-                  Text(
-                    "Include",
-                    style: TextStyle(fontSize: 30, color: fontColor),
-                  ),
-                  Row(
-                    children: [
-                      Transform.scale(
-                        scale: 1.4,
-
-                        child: Checkbox(
-                          fillColor: WidgetStateProperty.resolveWith((states) {
-                            if (states.contains(WidgetState.selected)) {
-                              return buttonsColor;
-                            }
-                            return null;
-                          }),
-                          checkColor: Colors.deepPurple,
-
-                          value: isChecked,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              isChecked = value;
-                            });
-                          },
-                        ),
-                      ),
-                      SizedBox(width: 15),
-                      Text(
-                        'Jobs shared with me',
-                        style: TextStyle(fontSize: 20, color: fontColor),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 100),
-                  SizedBox(
-                    height: 60,
-                    width: double.infinity,
-
-                    child: ElevatedButton(
+                    IconButton(
                       onPressed: () {},
-                      style: TextButton.styleFrom(
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(6),
-                        ),
-                        backgroundColor: Color(0xfFffbb57),
+                      icon: Icon(Icons.close, color: fontColor),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Text(
+                  "SEARCH BY KEYWORD",
+                  style: TextStyle(fontSize: 20, color: fontColor),
+                ),
+                SizedBox(
+                  width: double.infinity,
+                  height: 60,
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintStyle: TextStyle(color: fontColor),
+                      filled: true,
+
+                      fillColor: Color(0xFF242046),
+                      prefixIconColor: fontColor,
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(10),
                       ),
-                      child: Text(
-                        "Save Changes",
-                        style: TextStyle(fontSize: 20, color: Colors.black),
-                      ),
+                      hintText: "Search jobs",
+
+                      prefixIcon: Icon(Icons.search),
                     ),
                   ),
-                ],
-              ),
+                ),
+                SizedBox(height: 30),
+                Text(
+                  "View",
+                  style: TextStyle(fontSize: 30, color: fontColor),
+                ),
+                Row(
+                  children: [
+                    Transform.scale(
+                      scale: 1.4,
+                      child: Radio<Choices>(
+                        value: Choices.choice1,
+                        groupValue: _choice,
+                        activeColor: buttonsColor,
+                        onChanged: (Choices? value) {
+                          setState(() {
+                            _choice = value;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Text(
+                      'All job posts',
+                      style: TextStyle(fontSize: 20, color: fontColor),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Transform.scale(
+                      scale: 1.4,
+
+                      child: Radio<Choices>(
+                        value: Choices.choice2,
+                        groupValue: _choice,
+                        activeColor: buttonsColor,
+                        onChanged: (Choices? value) {
+                          setState(() {
+                            _choice = value;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Text(
+                      'Active job posts',
+                      style: TextStyle(fontSize: 20, color: fontColor),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    Transform.scale(
+                      scale: 1.4,
+                      child: Radio<Choices>(
+                        activeColor: buttonsColor,
+                        value: Choices.choice3,
+                        groupValue: _choice,
+                        onChanged: (Choices? value) {
+                          setState(() {
+                            _choice = value;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Text(
+                      'Archive job Posts',
+                      style: TextStyle(fontSize: 20, color: fontColor),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Text(
+                  "Include",
+                  style: TextStyle(fontSize: 30, color: fontColor),
+                ),
+                Row(
+                  children: [
+                    Transform.scale(
+                      scale: 1.4,
+
+                      child: Checkbox(
+                        fillColor: WidgetStateProperty.resolveWith((states) {
+                          if (states.contains(WidgetState.selected)) {
+                            return buttonsColor;
+                          }
+                          return null;
+                        }),
+                        checkColor: Colors.deepPurple,
+
+                        value: isChecked,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked = value;
+                          });
+                        },
+                      ),
+                    ),
+                    SizedBox(width: 15),
+                    Text(
+                      'Jobs shared with me',
+                      style: TextStyle(fontSize: 20, color: fontColor),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 100),
+                SizedBox(
+                  height: 60,
+                  width: double.infinity,
+
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: TextButton.styleFrom(
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(6),
+                      ),
+                      backgroundColor: Color(0xfFffbb57),
+                    ),
+                    child: Text(
+                      "Save Changes",
+                      style: TextStyle(fontSize: 20, color: Colors.black),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ),
